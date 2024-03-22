67771b3c9c62a622be84f9e6de34f64b203b40d5
nessy254 <mukaminessy9@gmail.com>
2024-03-22 20:18:20 +0300

    ssh configuration

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#using Puppet to make changes to our configuration file
+
+file_line { 'Turn off passwd auth':
+  path   => '/etc/ssh/sshd_config',
+  line   => 'PasswordAuthentication no',
+  ensure => present,
+}
+
+file_line { 'Declare identity file':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  ensure => present,
+}
+
