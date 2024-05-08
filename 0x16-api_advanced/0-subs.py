df65f7fa146fffe0f3f373d2a9262d57d3cb56e6
nessy254 <mukaminessy9@gmail.com>
2024-05-08 16:49:50 +0300

    a function that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,22 +3,24 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    a function that queries the Reddit API and
-    returns the number of subscribers
+    A function that queries the Reddit API and
+    returns the number of subscribers for a subreddit.
     """
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
-    headers  = {'User-Agent':"Chrome/98.0.4758.102"}
-    response = requests.get(url, headers = headers)
+    headers = {'User-Agent': 'Chrome/98.0.4758.102'}
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         info = response.json()
         subscribers = info['data']['subscribers']
-
-        return (subscribers)
-    else:
+        return subscribers
+    elif response.status_code == 404:
+        # Subreddit not found
         return 0
-
-
+    else:
+        # Handle other errors
+        print("Error:", response.status_code)
+        return None
 
