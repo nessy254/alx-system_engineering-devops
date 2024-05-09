e358f88b8ee9266a0ac77037c437ecb16558c0a2
nessy254 <mukaminessy9@gmail.com>
2024-05-09 05:58:18 +0300

    puppet

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# makes a typo correction in wordpress settings and fixes 500 error
+
+exec { 'fix_typo':
+  command =>   "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    =>   '/bin'
+}
